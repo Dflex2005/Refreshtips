d936ff8552c2e098a4e2ca9f06749f12a1d9a06c
Dflex2005 <davidtestimony8@gmail.com>
2022-06-10 03:08:33 +0100

    js update

--- topics/app.js
+++ topics/app.js
@@ -2,7 +2,7 @@ const scrollBtn = document.querySelector(".scroll-btn");
 const header = document.querySelector("header");
 window.addEventListener("scroll", ()=>{
     scrollBtn.classList.toggle("active", window.scrollY > 500);
-    header.classList.toggle("active", window.scrollY > 50);
+    header.classList.toggle("active", window.scrollY > 100);
 });
 
 scrollBtn.addEventListener("click", ()=>{
